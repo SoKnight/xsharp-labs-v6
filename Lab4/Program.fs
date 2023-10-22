e905175b11829435eaf9f44b6575763f984824bd
Kostya Permyakov <mcliyanage2018@gmail.com>
2023-10-22 20:33:08 +0500

    Upload incomplete lab4

---
+++ Lab4/Program.fs
@@ -0,0 +1,7 @@
+﻿// Вариант 6.
+//
+// 1 задание - Task1
+// 2 задание - Task2
+
+[<EntryPoint>]
+let main argv = Task1.run
---
+++ Lab4/Task1.fs
@@ -0,0 +1,44 @@
+﻿module Task1
+
+type Tree =
+    | Node of string * Tree * Tree
+    | Empty
+
+let rec printTree0 (tree: Tree) =
+    match tree with
+    | Node (data, left, right) ->
+        printf "%s ( " data
+        printTree0 (left)
+        printf " ) ( "
+        printTree0 (right)
+        printf " )"
+    | Empty -> ()
+
+let printTree (tree: Tree) =
+    printTree0 tree
+    printfn ""
+
+let run =
+    printfn "Задание 1:\nДерево содержит строки.\nЗаменить в каждой строке каждый символ на следующий по порядку.\n"
+
+    let tree: Tree = 
+        Node("root",
+            Node ("left", 
+                Node ("child1", Empty, Empty),
+                Empty
+            ),
+            Node ("right",
+                Node ("child2", Empty, Empty),
+                Node ("child3", Empty, Empty)
+            )
+        )
+
+    printfn "Исходное дерево:"
+    printTree tree
+
+    // how to use #map() here?..
+    let result = tree
+
+    printfn "\nРезультат:"
+    printTree result
+    0
