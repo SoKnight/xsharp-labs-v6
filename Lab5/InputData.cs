4458f3b5d15c68f6138f11545d553af2444f462e
Kostya Permyakov <mcliyanage2018@gmail.com>
2023-11-25 02:14:00 +0500

    Complete second task in lab 5

---
+++ Lab5/InputData.cs
@@ -0,0 +1,51 @@
+﻿namespace Lab5
+{
+    internal class InputData
+    {
+        // запрос ввода целого числа с проверкой
+        public static int RequestInteger(string prompt)                                                                                        
+        {
+            bool success;
+            int value;
+
+            do
+            {
+                Console.Write(prompt);
+                success = int.TryParse(Console.ReadLine(), out value);
+                if (!success)
+                {
+                    ConsoleColor tmp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Введенные данные имеют неверный формат!");
+                    Console.Write("Укажите целое число: ");
+                    Console.ForegroundColor = tmp;
+                }
+            } while (!success);
+
+            return value;
+        }
+
+        // запрос ввода вещественного числа с проверкой
+        public static double RequestDouble(string prompt)
+        {
+            bool success;
+            double value;
+
+            do
+            {
+                Console.Write(prompt);
+                success = double.TryParse(Console.ReadLine(), out value);
+                if (!success)
+                {
+                    ConsoleColor tmp = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Введенные данные имеют неверный формат!");
+                    Console.Write("Укажите вещественное число: ");
+                    Console.ForegroundColor = tmp;
+                }
+            } while (!success);
+
+            return value;
+        }
+    }
+}
---
+++ Lab5/Task2.cs
@@ -0,0 +1,79 @@
+﻿// Реализовать определение класса (поля, свойства, конструкторы, перегрузка метода ToString() для вывода полей, заданный метод согласно варианту).
+// Протестировать все методы, включая конструкторы, не забывайте проверять вводимые пользователем данные на корректность!
+
+// Вычисление корней квадратного уравнения.
+// Результат должен быть массивом величин типа double (в нем от 0 до 2-х элементов, в зависимости от количества корней).
+
+namespace Lab5
+{
+    // класс, описывающий квадратное уравнение в виде набора коэффициентов
+    public class QuadraticEquation
+    {
+        private readonly double a, b, c;
+
+        // конструктор со всеми коэффициентами
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        // метод для решения уравнения (по заданию)
+        public double[] SolveEquation()
+        {
+            double discriminant = (b * b) - (4 * a * c);
+
+            if (discriminant < 0.0)
+                return new double[0];
+
+            if (discriminant == 0.0)
+                return new double[] { -b / (2 * a) };
+
+            double dsqrt = Math.Sqrt(discriminant);
+
+            return new double[]
+            {
+                (-b - dsqrt) / (2 * a),
+                (-b + dsqrt) / (2 * a)
+            };
+        }
+
+        // перегрузка #ToString()
+        public override string ToString()
+        {
+            return "{a=" + a
+                + ", b=" + b
+                + ", c=" + c
+                + "}";
+        }
+    }
+
+    internal class Task2
+    {
+        public static void Run()
+        {
+            Console.WriteLine("--- Квадратное уравнение ---");
+            double a = InputData.RequestDouble("Введите коэффициент при x^2: ");
+            double b = InputData.RequestDouble("Введите коэффициент при x: ");
+            double c = InputData.RequestDouble("Введите свободный коэффициент: ");
+            Console.WriteLine();
+
+            QuadraticEquation equation = new QuadraticEquation(a, b, c);
+            double[] roots = equation.SolveEquation();
+
+            switch (roots.Length)
+            {
+                case 0:
+                    Console.WriteLine("Данное уравнение не имеет решений!");
+                    break;
+                case 1:
+                    Console.WriteLine($"Уравнение имеет одно решение: {roots[0]}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Уравнение имеет два решения: {roots[0]} и {roots[1]}");
+                    break;
+            }
+        }
+    }
+}
